2c1d46ddd9a205810d20523e8b2765ff9d26da28
LucasViana18 <lucasvviana14@gmail.com>
2019-11-30 16:08:16 +0000

    Add a start menu

--- MovieDatabase/Program.cs
+++ MovieDatabase/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.IO;
 
 namespace MovieDatabase
 {
     class Program
     {
+        private const string folderName = "MyIMDBSearcher";
+        private const string fileTitleBasics = "title.basics.tsv";
+        private const string fileTitleRatings = "title.ratings.tsv";
+        private const string fileTitleEpisodes = "title.episode.tsv";
+        private const string fileNameBasics = "name.basics.tsv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string folderPath = Path.Combine(
+            Environment.GetFolderPath
+            (Environment.SpecialFolder.LocalApplicationData), folderName);
+
+            string fileTitleBasicsPath = 
+                Path.Combine(folderPath, fileTitleBasics);
+            string fileTitleRatingsPath = 
+                Path.Combine(folderPath, fileTitleRatings);
+            string fileTitleEpisodesPath = 
+                Path.Combine(folderPath, fileTitleEpisodes);
+            string fileNameBasicsPath = 
+                Path.Combine(folderPath, fileNameBasics);
+
+            Console.WriteLine("Selecione uma opção: \n 1 - Títulos\n " +
+                "2 - Pessoas\n 3 - Sair");
+
+            string response = Console.ReadLine();
+            bool a = true;
+
+            while (a)
+            {
+                switch (response)
+                {
+                    case "1":
+                        a = false;
+                        // Call method from Title class
+                        break;
+                    case "2":
+                        a = false;
+                        // Call method from Person class
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Digite uma das opções disponíveis");
+                        break;
+                }
+            }
         }
     }
 }

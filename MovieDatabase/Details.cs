a037134031e7441bf0569d2e9c09ea8643466d86
LucasViana18 <lucasvviana14@gmail.com>
2019-12-13 00:35:21 +0000

    Add Details class

---
+++ MovieDatabase/Details.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieDatabase
+{
+    public class Details
+    {
+        // Properties
+        public string Tconst { get; set; }
+        public string TitleType { get; set; }
+        public string PrimaryTitle { get; set; }
+        public string OriginalTitle { get; set; }
+        public string IsAdult { get; set; }
+        public string StartYear { get; set; }
+        public string EndYear { get; set; }
+        public string RuntimeMinutes { get; set; }
+        public string Genres { get; set; }
+        public string AverageRating { get; set; }
+        public string numVotes { get; set; }
+        public string ID { get; private set; }
+
+        // Constructor
+        public Details(string[] array)
+        {
+            // Each category has stored an index of the array
+            Tconst = array[0];
+            TitleType = array[1];
+            PrimaryTitle = array[2];
+            OriginalTitle = array[3];
+            IsAdult = array[4];
+            StartYear = array[5];
+            EndYear = array[6];
+            RuntimeMinutes = array[7];
+            Genres = array[8];
+            AverageRating = array[9];
+            numVotes = array[10];
+            ID = array[11];
+        }
+    }
+}

3cc812f2bd378eb49bf7504acb75e1018473b652
Rita Simões Saraiva <belgas2000@gmail.com>
2019-12-16 22:50:09 +0000

    add FilterType and DbType enum

---
+++ MovieDatabase/DbType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieDatabase
+{
+    public enum DbType
+    {
+        dtTitles,
+        dtRatings,
+        dtEpisodes,
+        dtPersons,
+        dtAll
+    }
+}
---
+++ MovieDatabase/FilterType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieDatabase
+{
+    public enum FilterType
+    {
+        ftTitlesByName,
+        ftTitlesByPerson,
+        ftSerieForEpisode,
+        ftEpisodesForSerie,
+        ftPersonsByName,
+        ftPersonsByTitle
+    }
+}
